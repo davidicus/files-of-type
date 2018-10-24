c62dcd50c23a66df53f5a2f8d72fa48554198b56
davidicus <david@david-conner.com>
2018-10-24 14:46:47 -0500

    :sparkles: added function to get files of specific type

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 "use strict";
+
+const promisify = require("util").promisify;
+const path = require("path");
+
 /**
  * This function does something.
- * @param input Some param description.
- * @param options An object of options.
- * @returns something userful.
+ * @param fileType - the file extension you are searching for
+ * @param filePath - the path to directory you would like to search. Defaults to __dirname
+ * @returns <Promise> which will eventually resolve to an array with either the files names or empty
  */
-module.exports = (input, options = {}) => {};
+module.exports = (fileType, filePath = "./") => {
+  const match = "([a-zA-Z0-9\\s_\\.\\-():])+(";
+  const regEx = new RegExp(match + fileType + ")$", "i");
+  const fsReadDir = promisify(fs.readdir);
+  const directoryPath = path.resolve(__dirname, filePath);
+  return fsReadDir(directoryPath).then(files =>
+    files.filter(file => file.match(regEx))
+  );
+};
